7be8998e2893858efac55d044e065fb8d09a121d
lauranimo <lauren.ouyang@gmail.com>
2022-07-13 16:50:35 -0500

    moved into separate component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,15 @@
 import React, { useState } from "react"
-import Widget from "../components/Widget";
-import TaskHeader from "../components/todo/TaskHeader"
-import Tasks from '../components/todo/Tasks';
 import '../components/index.css'
-import AddTask from '../components/todo/AddTask';
-import Time from '../components/background/Time';
-import cafeAnimation from "../images/cafe-animation.mp4"
-import AllSounds from '../components/bg-sound/AllSounds';
-import Timer from "../components/timer/Timer";
+import Layout from "../components/Layout"
+
 
 
 function Index() {
-  const [todo, setTodo] = useState(false);
-  const [sound, setSound] = useState(false);
-
-
-  // We need the tasks variable across components- can't declare inside just one
-  const [showForm, setShowForm] = useState(false)
-  const [tasks, setTasks] = useState([])
-
-  // Add Task- might generate same number lol
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() *1000) + 1
-    const newTask = {id, ...task}
-    setTasks([...tasks, newTask])
-  }
-
-  // Delete Task
-  const deleteTask = (id) => {
-    setTasks(tasks.filter(task => task.id !== id))
-  }
-
-  // Toggle Importantance
-  const toggleimportant = (id) => {
-    console.log(id)
-    setTasks(tasks.map((task) => task.id === id ?
-    {...task, important: !task.important} : task))
-  }
 
 
   return (
     <main>
-        <Widget id = {'todo-widget'}
-          container = {<button className={`todo-container ${todo ? 'focus' : ''}`}  onClick={() => setTodo(!todo)} key='contain'>
-            <TaskHeader onAdd={() => setShowForm(!showForm)}
-            showForm={showForm}/>
-            {showForm && <AddTask onAdd={addTask}/>}
-            <Tasks tasks = {tasks}
-              onDelete = {deleteTask}
-              onToggle = {toggleimportant}/>     
-            </button>}>
-        </Widget>
-      <div className='centerpiece'>
-        <video autoPlay loop muted id='animation'>
-          <source src={cafeAnimation} type='video/mp4'/>
-        </video>      
-        <div className="greetings-container">
-          <Time/>
-        </div>
-      </div>
-
-      <Widget id = {'sounds-widget'}
-        container = {<button className={`sounds-container ${sound ? 'focus' : ''}`}  onClick={() => setSound(!sound)}>
-          <header className='header'>
-            <h1>Sounds</h1>
-          </header>
-          <AllSounds/>
-        </button>}>
-      </Widget>
-      <Widget id = {'timer-widget'}
-        container = {<button className={'timer-container'}><Timer/></button>}>
-        </Widget>
+       <Layout></Layout>
     </main>
   )
 }
